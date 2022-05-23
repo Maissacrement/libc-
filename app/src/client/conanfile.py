35f28ac9e3b6ae89121e3b70fd743ed273a7dd53
delar <emmariodelar@gmail.com>
2022-05-23 20:57:31 +0200

    Conan client upgrade

---
+++ app/src/client/conanfile.py
@@ -0,0 +1,40 @@
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+class CurlpatternConan(ConanFile):
+    name = "Curlpattern"
+    version = "0.1"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt"
+
+    generators = "cmake"
+    requires = ["libcurl/7.80.0","nlohmann_json/3.10.5"]
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
